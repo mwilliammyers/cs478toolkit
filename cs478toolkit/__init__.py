4add8c98096e119559fa9a229ad7f218fd12e7c7
mwilliammyers <mwilliammyers@gmail.com>
2017-02-23 19:18:28 -0700

    Reformat code

--- cs478toolkit/__init__.py
+++ cs478toolkit/__init__.py
@@ -3,23 +3,6 @@ import arff
 import numpy as np
 import logging
 
-# import os.path
-# import site
-#
-# plugins = []
-#
-# def register_plugin(path):
-#     plugins.append(path)
-#
-# def load_plugin(file):
-#     site.addsitedir(os.path.dirname(file))
-#     return __import__(os.path.splitext(os.path.basename(file))[0])
-#
-# def call_plugin(name, *args, **kwargs):
-#     plugin = load_plugin(name)
-#     plugin.train(*args, **kwargs)
-#
-# call_plugin("/Users/wm/Documents/byu/cs478/cs478-labs.tmp/backpropagation/bp.py")
 
 def _log_level(count):
     levels = [logging.WARNING, logging.INFO, logging.DEBUG]
@@ -27,15 +10,19 @@ def _log_level(count):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Machine Learning Toolkit")
+    parser = argparse.ArgumentParser(description="Toolkit for BYU CS 478")
 
-    parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase verbosity')
-    # parser.add_argument('-N', '--normalize', action='store_true', help='Use normalized data')
     parser.add_argument(
-        '-R', '--seed', help='Random seed')  # will give a string
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        help='Increase verbosity')
+    # parser.add_argument('-N', '--normalize', action='store_true', help='Use normalized data')
+    # parser.add_argument('-R', '--seed', help='Random seed')
     # parser.add_argument('-L', required=True, choices=['baseline', 'perceptron', 'neuralnet', 'decisiontree', 'knn'], help='Learning Algorithm')
     parser.add_argument(
-        '-A', '--arff', metavar='filename', required=True, help='ARFF file')
+        '-A', '--arff', metavar='filename', required=True, help='Path to ARFF file to load')
     # parser.add_argument('-E', metavar=('METHOD', 'args'), required=True, nargs='+', help="Evaluation method (training | static <test_ARFF_file> | random <%%_for_training> | cross <num_folds>)")
 
     return parser.parse_args()
@@ -49,7 +36,10 @@ def load_data(file, add_bias=True, encode_nominal=True):
     return data
 
 
-def initialize(log_format="%(filename)s:%(lineno)s:%(funcName)s():\n%(message)s", args_parser=parse_args, data_loader=load_data):
+def initialize(
+        log_format="%(filename)s:%(lineno)s:%(funcName)s():\n%(message)s",
+        args_parser=parse_args,
+        data_loader=load_data):
     args = args_parser()
     logging.basicConfig(format=log_format, level=_log_level(args.verbose))
     return data_loader(args.arff)
