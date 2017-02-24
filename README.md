328d805af749dc853253a5b97f2f4e55fd72e324
mwilliammyers <mwilliammyers@gmail.com>
2017-02-23 19:13:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# cs478toolkit
+
+Basic machine learning toolkit for BYU CS478
+
+## requirements
+
+- [python2.7 or python3.3+](https://www.python.org/downloads/)
+- [pip](https://pip.pypa.io/en/stable/installing/) (_optional_)
+
+## installation 
+
+```
+pip install git+https://github.com/mwilliammyers/cs478toolkit
+```
+
+## usage
+
+```python
+import cs478toolkit
+
+data = cs478toolkit.initialize()
+```
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cs478toolkit',
+    version='0.1.0',
+    description='Basic machine learning toolkit for BYU CS478',
+    url='http://github.com/mwilliammyers/cs478',
+    author='mwilliammyers',
+    author_email='mwilliammyers@gmail.com',
+    license='MIT',
+    packages=['cs478toolkit'],
+    install_requires=[
+        'liac-arff',
+        'numpy'
+    ],
+    zip_safe=False)
