f32c73b94af936f61e7c51d6b071be6657682050
mwilliammyers <mwilliammyers@gmail.com>
2017-03-03 21:29:12 -0700

    Fix __future__ statements

--- cs478toolkit/__init__.py
+++ cs478toolkit/__init__.py
@@ -1,5 +1,6 @@
 from .visual import bar, plot
 from .cli import parse_args
 from .data import load, shuffle, split
+import measure
 
 __all__ = ['bar', 'plot', 'parse_args', 'load', 'shuffle', 'split']
