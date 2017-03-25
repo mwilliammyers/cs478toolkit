22427de1aa9c27bba76333751b8a93dd74e627f7
mwilliammyers <mwilliammyers@gmail.com>
2017-03-24 19:12:57 -0600

    Add scipy dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email='mwilliammyers@gmail.com',
     license='MIT',
     packages=['lathe'],
-    install_requires=['liac-arff', 'numpy', 'scikit-learn'],
+    install_requires=['liac-arff', 'numpy', 'scikit-learn', 'scipy'],
     keywords=['machine learning', 'ml', 'toolkit', 'byu', 'cs478'],
     zip_safe=False)
